26f2f0fec2cb7bb19f8dd7efd8152ea66ea3a042
jysylee <jysylvia.lee@gmail.com>
2022-05-27 21:20:48 -0400

    quote & background 완성 / To-do List 시작

--- js/quotes.js
+++ js/quotes.js
@@ -1,47 +1,49 @@
 const quotes = [
     {
-        quotes: "What would life be if we had no courage to attempt anything?",
+        quote: "What would life be if we had no courage to attempt anything?",
         author: "Vincent van Gogh"
     },
     {
-        quotes: "You are a human with one life and it is up to you to make it the best life you can.",
+        quote: "You are a human with one life and it is up to you to make it the best life you can.",
         author: "Dan Howell"
     },
     {
-        quotes: "Life isn't about finding yourself. Life is about creating yourself.",
+        quote: "Life isn't about finding yourself. Life is about creating yourself.",
         author: "George Bernard Shaw"
     },
     {
-        quotes: "You can't please everyone, and you can't make everyone like you.",
+        quote: "You can't please everyone, and you can't make everyone like you.",
         author: "Katie Couric"
     },
     {
-        quotes: "There are no mistakes, only opportunities.",
+        quote: "There are no mistakes, only opportunities.",
         author: "Tina Fey"
     },
     {
-        quotes: "Once you choose hope, anything's possible.",
+        quote: "Once you choose hope, anything's possible.",
         author: "Christopher Reeve"
     },
     {
-        quotes: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+        quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
         author: "Mahatma Gandhi"
     },
     {
-        quotes: "Life is ten percent what happens to you and ninety percent how you respond to it.",
+        quote: "Life is ten percent what happens to you and ninety percent how you respond to it.",
         author: "Lou Holtz"
     },
     {
-        quotes: "In three words I can sum up everything I’ve learned about life: it goes on.",
+        quote: "In three words I can sum up everything I’ve learned about life: it goes on.",
         author: "Robert Frost"
     },
     {
-        quotes: "People are just about as happy as they make up their minds to be.",
+        quote: "People are just about as happy as they make up their minds to be.",
         author: "Abraham Lincoln"
     }
 ];
 
-const quote = document.querySelector("$quote span:first-child");
-const author = document.querySelector("$quote span:last-child");
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-console.log(quotes[0]);
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
